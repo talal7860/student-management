a9e2e1c29b44a55a2346a0e801a51833851fd1f2
Usama <usama.lhr@gmail.com>
2017-01-24 14:10:36 +0500

    setting default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = 'student-management-system_#{Rails.env}'
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = {host: 'localhost'}
+  config.action_mailer.default_url_options = {host: 'herokuapp.com'}
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings =
   # {
